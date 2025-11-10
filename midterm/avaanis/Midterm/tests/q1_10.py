904c7170622bde630cbd095844d8da7127102b30
Rayna Patel <raynapatel@Raynas-MacBook-Air-5.local>
2025-11-09 23:35:55 -0800

    pushing midterm files

---
+++ midterm/avaanis/Midterm/tests/q1_10.py
@@ -0,0 +1,12 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_10',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> type(most_popular) == pd.Series\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
